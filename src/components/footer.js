0291a090eff54e1658c6bbbf81fb66b8416f13c9
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2022-07-21 09:58:09 -0600

    changes to layouts and styling for mobile usage

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,8 +9,11 @@ import { FaLocationArrow } from 'react-icons/fa'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      <h4>Come by and see us! <br/> <FaLocationArrow /><a href="https://www.google.com/maps/place/1510+Deborah+Rd+SE,+Rio+Rancho,+NM+87124/@35.2358491,-106.6650248,15z/data=!3m1!4b1!4m5!3m4!1s0x872270e657085d49:0xa9449a209b0cb79d!8m2!3d35.2358319!4d-106.6562915">1510 Deborah Rd SE Rio Rancho, NM 87124</a> </h4>
-      Give us a call <a href="tel:+1-505-892-5243">(505)-892-5243</a>
+      <h4>Come by and see us!</h4>
+      <FaLocationArrow />
+      <a href="https://www.google.com/maps/place/1510+Deborah+Rd+SE,+Rio+Rancho,+NM+87124/@35.2358491,-106.6650248,15z/data=!3m1!4b1!4m5!3m4!1s0x872270e657085d49:0xa9449a209b0cb79d!8m2!3d35.2358319!4d-106.6562915" className="address">1510 Deborah Rd SE Rio Rancho, NM 87124</a> <br/>
+     <p> Give us a call</p> 
+     <a href="tel:+1-505-892-5243">(505)-892-5243</a>
       <p>Check us out on social media!</p>
       <a
         href="https://www.facebook.com/tjsevergreen"
@@ -28,7 +31,7 @@ const Footer = () => (
       >
         <FaInstagram style={{ fontSize: '32px', margin: '2%' }}></FaInstagram>
       </a>
-      <p className="tradeMark"> &copy;2020 All rights reserved.</p>
+      <p> &copy;2020 All rights reserved.</p>
     </div>
   </Container>
 )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,11 +9,11 @@ const contact = () => {
       <div class="contact">
         <h3>Hours of operation</h3>
         <ul>
-          <li>Monday - Friday 10:00 Am - 5:30 Pm</li>
-          <li>Saturday 10:00 Am - 5:00 Pm </li>
-          <li>Sunday Closed </li>
+          <ol>Monday - Friday 10:00 Am - 5:30 Pm</ol>
+          <ol>Saturday 10:00 Am - 5:00 Pm </ol>
+          <ol>Sunday Closed </ol>
         </ul>
-        <form name="contac" netlify>
+        <form name="contact" netlify>
         <h4 style={{textAlign: 'center', marginTop: '1%'}}>Have a question? <br /> Feel free to reach out to us!</h4>
           <p>
             <label>
